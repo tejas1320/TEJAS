8c829f03f297857302d1c8a1898b375b9ece4271
tejas1320 <tejass.ds2024@vemanait.edu.in>
2026-03-14 20:48:57 +0530

    Time: 16 ms (33.07%) | Memory: 14.4 MB (21.10%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,23 @@
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> seen;
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (seen.count(s[right])) {
+                seen.erase(s[left]);
+                left++;
+            }
+
+            seen.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
